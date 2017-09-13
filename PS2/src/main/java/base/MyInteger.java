4ec396e8eceaf0f6a63b6eeb3a0592e3250efa8e
gaoji <gaoji@128.4.101.108>
2017-09-13 13:04:44 -0400

    init commit

---
+++ PS2/src/main/java/base/MyInteger.java
@@ -0,0 +1,17 @@
+package base;
+
+public class MyInteger {
+	
+	private int iValue;
+	public MyInteger(int iValue) {
+		this.iValue = iValue;
+	}
+	public int getiValue() {
+		return iValue;
+	}
+	
+	
+	
+	
+
+}
